22cf976b0dc6dc36ec380df66bd1d2167b12fa0d
Roope <roope.nahkala@gmail.com>
2024-02-12 15:04:43 +0200

    valmis

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainAppbar from './components/MainAppBar';
+import { ThemeContext } from './contexts/ThemeContext';
+import Settings from './screens/settings'
+import ThemeProvider from './contexts/ThemeProvider'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import  Home  from './screens/home.js'
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ThemeProvider>
+      <NavigationContainer>
+        <StatusBar backgroundColor="#666" barStyle="Light-content" />
+        <Stack.Navigator
+          initialRouteName='Home'
+          screenOptions={{header: (props) =>
+              <MainAppbar {...props} backgroundColor="#666" icon="cog" color="#fff" />
+          }}
+        >
+          <Stack.Screen name="Home">
+            {() =>
+              <Home />
+            }
+          </Stack.Screen>
+          <Stack.Screen name="Settings" component={Settings} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      </ThemeProvider>
+    </PaperProvider>
   );
 }
 
@@ -14,7 +39,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
